6d53be2dedf61fe8ddec4a515aad2463b0361ccc
r.ostapchuk <kucheriavii@gmail.com>
2024-01-30 21:59:17 +0200

    017 Monsters Rolodex - setState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react'
+class App extends Component{
+  constructor(){
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      name: 'Roman'
+    }
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi, {this.state.name}!!!
+          </p>
+          <button onClick={() => {
+            this.setState({name:"Vasia"})            
+          }}>Change name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
